df727723b96a623d641ad8c9e5b6a5079bdb4df9
wfilipunas <wfilipunas@gmail.com>
2021-05-21 00:37:33 +0200

    Działania

---
+++ js/main.js
@@ -0,0 +1,49 @@
+function dzialania(a,b) {
+    let suma = a + b;
+    let roznica = a - b;
+    let iloczyn = a * b;
+    let textSuma = 'Wynik dodawania wynosi '
+    let textRoznica = 'Wynik odejmowania wynosi ';
+    let textIloczyn = 'Wynik mnożenia wynosi ';
+    
+    if (suma >= 0 && roznica >= 0 && iloczyn >= 0 ) {
+        console.log(textSuma.substr(0) + suma);
+        console.log(textRoznica.substr(0) + roznica);
+        console.log(textIloczyn.substr(0) + iloczyn);
+    } else if (suma < 0 && roznica > 0 && iloczyn > 0){
+        suma = console.log('Wynik nieprawidłowy');
+        console.log(textRoznica.substr(0) + roznica);
+        console.log(textIloczyn.substr(0) + iloczyn);
+    } else if (suma > 0 && roznica < 0 && iloczyn > 0) {
+        console.log(textSuma.substr(0) + suma);
+        roznica = console.log('Wynik nieprawidłowy');
+        console.log(textIloczyn.substr(0) + iloczyn);
+    } else if (iloczyn  < 0 && suma > 0 && roznica > 0) {
+        console.log(textSuma.substr(0) + suma);
+        console.log(textRoznica.substr(0) + roznica);
+        iloczyn = console.log('Wynik nieprawidłowy');
+    } else if (suma > 0 && roznica < 0 && iloczyn < 0) {
+        console.log(textSuma.substr(0) + suma);
+        roznica = console.log('Wynik nieprawidłowy');
+        iloczyn = console.log('Wynik nieprawidłowy');
+    } else if (suma < 0 && roznica > 0 && iloczyn < 0) {
+        suma = console.log('Wynik nieprawidłowy');
+        console.log(textRoznica.substr(0) + roznica);
+        iloczyn = console.log('Wynik nieprawidłowy');
+    } else if (suma < 0 && roznica < 0 && iloczyn < 0) {
+        suma = console.log('Wynik nieprawidłowy');
+        roznica = console.log('Wynik nieprawidłowy');
+        iloczyn = console.log('Wynik nieprawidłowy');
+    } else if (suma < 0 && roznica > 0 && iloczyn > 0) {
+        suma = console.log('Wynik nieprawidłowy');
+        console.log(textRoznica.substr(0) + roznica);
+        console.log(textIloczyn.substr(0) + iloczyn);
+    } else if (suma > 0 && roznica > 0 && iloczyn < 0) {
+        console.log(textSuma.substr(0) + suma);
+        console.log(textRoznica.substr(0) + roznica);
+        iloczyn = console.log('Wynik nieprawidłowy');
+    }
+}
+
+dzialania(1,3);
+
